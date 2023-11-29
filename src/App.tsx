01f3dd327f9a88929ac5e7623a2aa56c0cf25e85
Andres Gutierrez <gagutierrez454@soy.sena.edu.co>
2023-11-29 17:35:13 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
+import { useEffect } from 'react';
 import Cookies from 'js-cookie';
 import './App.css';
 
 function App() {
   Cookies.remove('sessionToken');
+
   const resizeIframe = (iframe: HTMLIFrameElement) => {
     iframe.height = iframe.contentWindow?.document.body.scrollHeight + "px";
   }
+
   const createFrameFAB = () => {
     return(
       <iframe src="https://www.lifestylecalculator.com" className="w-screen" onLoad={(e) => resizeIframe(e.target as HTMLIFrameElement)} ></iframe>
     );
   }
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      window.location.reload();
+    }, 180000);
+  
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className="container">
       {createFrameFAB()}
